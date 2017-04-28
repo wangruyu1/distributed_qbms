0a93f5e5c71a07e708df9f90ebe17756ea36858f
wangruyu1 <2534499aa>
2017-04-28 10:08:29 +0800

    优化菜单查询实现无限极菜单.

--- qbms_pc/src/main/java/cn/qtech/service/impl/MenuServiceImpl.java
+++ qbms_pc/src/main/java/cn/qtech/service/impl/MenuServiceImpl.java
@@ -6,10 +6,10 @@ import cn.qtech.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author wangruyu
@@ -28,22 +28,30 @@ public class MenuServiceImpl implements MenuService {
     @Override
     public List<Menu> queryTreeMenusByRoleIds(List<String> roleIds) {
         List<Menu> allMenus = this.queryMenusByRoleIds(roleIds);
-        Map<String, Menu> menuMap = new HashMap<>();
+        Map<String, Menu> menuMap = allMenus.stream().collect(Collectors.toMap(Menu::getId, (p) -> p));
+        List<Menu> rootMenu = new ArrayList<>();
+        menuMap.forEach((menuId, menu) -> {
+            if (menu.getParentid() != null && !"".equals(menu.getParentid())) {
+                menuMap.get(menu.getParentid()).getSubMenus().add(menu);
+            } else {
+                rootMenu.add(menu);
+            }
+        });
         //获取所有父级菜单
-        if (allMenus != null && allMenus.size() > 0) {
-            allMenus.forEach(menu -> {
-                if (menu.getParentid() == null || menu.getParentid().equals("")) {
-                    menuMap.put(menu.getId(), menu);
-                }
-            });
-            allMenus.forEach(menu -> {
-                if (menu.getParentid() != null && !menu.getParentid().equals("")) {
-                    String pid = menu.getParentid();
-                    menuMap.get(pid).getSubMenus().add(menu);
-                }
-            });
-        }
-
-        return Arrays.asList(menuMap.values().toArray(new Menu[menuMap.size()]));
+//        if (allMenus != null && allMenus.size() > 0) {
+//            allMenus.forEach(menu -> {
+//                if (menu.getParentid() == null || menu.getParentid().equals("")) {
+//                    menuMap.put(menu.getId(), menu);
+//                }
+//            });
+//            allMenus.forEach(menu -> {
+//                if (menu.getParentid() != null && !menu.getParentid().equals("")) {
+//                    String pid = menu.getParentid();
+//                    menuMap.get(pid).getSubMenus().add(menu);
+//                }
+//            });
+//        }
+//        return Arrays.asList(menuMap.values().toArray(new Menu[menuMap.size()]));
+        return rootMenu;
     }
 }
